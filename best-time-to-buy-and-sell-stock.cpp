f79557248b7416fcd18ef62963ea1880efb31294
Yao <syao@hpc1.csc.bnl.gov>
2015-09-07 19:38:13 -0400

    best time to buy and sell stock

---
+++ best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,22 @@
+// LeetCode problem
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int n = prices.size();
+        if(n <= 1)
+            return 0;
+        // prices are >=0 integers.
+        int lowest = prices[0];
+        int res = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if (prices[i] < lowest) {
+                lowest = prices[i];
+            } else {
+                res = max(res, prices[i] - lowest);
+            }
+        }
+        return res;
+    }
+};

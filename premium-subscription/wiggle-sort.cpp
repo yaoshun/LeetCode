6aa1515a5638083cc83767bc5561a7d069968ae7
Yao <syao@hpc1.csc.bnl.gov>
2015-09-10 00:08:04 -0400

    wiggle sort updated

---
+++ premium-subscription/wiggle-sort.cpp
@@ -0,0 +1,23 @@
+// LeetCode problem
+//
+// Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+//
+// For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1)
+            return;
+        
+        // If 0~(i - 1) is already wiggled sorted,
+        // swap the value is enough to make 0~i wiggle sorted.
+        for (int i = 1; i < n; i++) {
+            if ((i % 2 == 0 && nums[i] > nums[i - 1]) || 
+                (i % 2 == 1 && nums[i] < nums[i - 1])) {
+                swap(nums[i - 1], nums[i]);
+            }
+        }
+    }
+};
